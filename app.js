f7bd8dca551b2737888f4f03e6df1b08f0db6392
moopmonster <moopmonster@gmail.com>
2018-12-27 17:08:13 +0800

    turn off dev logging

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.set('db_model', require('./lib/model/db'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
